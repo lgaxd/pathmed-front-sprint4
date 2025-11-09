56aefd0fd716b04952f729a61144d1ecddca7e91
lgaxd <lucasgrilloxd@gmail.com>
2025-11-09 17:36:10 -0300

    trocando link da api python para teste

--- src/contexts/api-context.tsx
+++ src/contexts/api-context.tsx
@@ -15,7 +15,7 @@ export const ApiProvider: React.FC<{ children: ReactNode }> = ({ children }) => 
 
   const apiBaseUrl = apiType === 'java' 
     ? 'https://pathmed.winty.cloud'
-    : 'https://pathmedapi.winty.cloud/api/v1';
+    : 'http://localhost:8000/api/v1';
 
   return (
     <ApiContext.Provider value={{ apiType, setApiType, apiBaseUrl }}>
